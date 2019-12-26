46f83ce5180f6f11709e1fd4709b04efffd55a7e
Galen Weber <galenweber@gmail.com>
2019-12-26 09:33:03 -0500

    fix bolt torque pattern image

--- src/components/torque-pattern-images/index.js
+++ src/components/torque-pattern-images/index.js
@@ -1,18 +1,18 @@
 import bolt4 from './4-bolt-torque-pattern';
-import bolt8 from './4-bolt-torque-pattern';
-import bolt12 from './4-bolt-torque-pattern';
-import bolt16 from './4-bolt-torque-pattern';
-import bolt20 from './4-bolt-torque-pattern';
-import bolt24 from './4-bolt-torque-pattern';
-import bolt28 from './4-bolt-torque-pattern';
-import bolt32 from './4-bolt-torque-pattern';
-import bolt36 from './4-bolt-torque-pattern';
-import bolt40 from './4-bolt-torque-pattern';
-import bolt44 from './4-bolt-torque-pattern';
-import bolt48 from './4-bolt-torque-pattern';
-import bolt52 from './4-bolt-torque-pattern';
-import bolt56 from './4-bolt-torque-pattern';
-import bolt60 from './4-bolt-torque-pattern';
+import bolt8 from './8-bolt-torque-pattern';
+import bolt12 from './12-bolt-torque-pattern';
+import bolt16 from './16-bolt-torque-pattern';
+import bolt20 from './20-bolt-torque-pattern';
+import bolt24 from './24-bolt-torque-pattern';
+import bolt28 from './28-bolt-torque-pattern';
+import bolt32 from './32-bolt-torque-pattern';
+import bolt36 from './36-bolt-torque-pattern';
+import bolt40 from './40-bolt-torque-pattern';
+import bolt44 from './44-bolt-torque-pattern';
+import bolt48 from './48-bolt-torque-pattern';
+import bolt52 from './52-bolt-torque-pattern';
+import bolt56 from './56-bolt-torque-pattern';
+import bolt60 from './60-bolt-torque-pattern';
 
 export default {
   bolt4,
